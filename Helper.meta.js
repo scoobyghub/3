6a7fca736f0e80e5d20e8ffbc7456bb7ccdc216c
Scooby <tmnsparky@hotmail.co.uk>
2026-03-17 17:09:30 +0000

    v14.04 Update Helper.meta.js

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         TMN TDS Auto v14.02
+// @name         TMN TDS Auto v14.04
 // @namespace    http://tampermonkey.net/
-// @version      14.02
-// @description  v14.02 — Human delays, OC/DTM 5-layer dedup, FOUC fix
+// @version      14.04
+// @description  v14.04 — Human delays, OC/DTM 5-layer dedup, FOUC fix
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
