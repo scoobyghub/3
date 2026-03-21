ea5087ee3ca40ed65f912848fd51ee9ed0dc8a29
Scooby <tmnsparky@hotmail.co.uk>
2026-03-21 17:21:28 +0000

    Update Helper.meta.js

--- Helper.meta.js
+++ Helper.meta.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         TMN TDS Auto v14.07
+// @name         TMN TDS Auto v14.08
 // @namespace    http://tampermonkey.net/
-// @version      14.07
-// @description  v14.07 — Human delays, OC/DTM 5-layer dedup, FOUC fix
+// @version      14.08
+// @description  v14.08 — Human delays, OC/DTM 5-layer dedup, FOUC fix
 // @author       You
 // @match        *://www.tmn2010.net/login.aspx*
 // @match        *://www.tmn2010.net/authenticated/*
